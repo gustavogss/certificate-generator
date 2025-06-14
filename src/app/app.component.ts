89473611d84e48ae0352e1593378f0f1184b03ea
gustavogss <gustavogss.dev@gmail.com>
2025-06-14 13:58:32 -0300

    fix: ajustes em app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { CommonModule } from '@angular/common';
 import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { SecondButtonComponent } from "./_components/second-button/second-button.component";
+import { ItemListComponent } from "./_components/item-list/item-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, CommonModule, PrimaryButtonComponent],
+  imports: [NavbarComponent, CommonModule, PrimaryButtonComponent, SecondButtonComponent, ItemListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
