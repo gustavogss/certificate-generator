fe289effbce55fe1a668c65533e2451abc34d6cb
gustavogss <gustavogss.dev@gmail.com>
2025-06-14 18:51:34 -0300

    feat: list certificates

--- src/app/pages/certificates/certificates.component.ts
+++ src/app/pages/certificates/certificates.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { SecondButtonComponent } from "../../_components/second-button/second-button.component";
+import { ItemListComponent } from "../../_components/item-list/item-list.component";
 
 @Component({
   selector: 'app-certificates',
-  imports: [SecondButtonComponent],
+  imports: [SecondButtonComponent, ItemListComponent],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css'
 })
