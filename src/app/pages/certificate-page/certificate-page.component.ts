0c9e97aee1a9adc742ec4b102dd6f46f2e275a3a
gustavogss <gustavogss.dev@gmail.com>
2025-06-15 00:34:03 -0300

    feat: created page certificate

---
+++ src/app/pages/certificate-page/certificate-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondButtonComponent } from "../../_components/second-button/second-button.component";
+
+@Component({
+  selector: 'app-certificate-page',
+  imports: [SecondButtonComponent],
+  templateUrl: './certificate-page.component.html',
+  styleUrl: './certificate-page.component.css'
+})
+export class CertificatePageComponent {
+
+}
