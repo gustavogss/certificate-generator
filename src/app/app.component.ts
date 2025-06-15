3c06b619bc2e043f5ffca0aa7ebcc2af36b9dc70
gustavogss <gustavogss.dev@gmail.com>
2025-06-15 00:37:34 -0300

    fix: project conflit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,11 @@ import { UiBaseComponent } from "./_components/ui-base/ui-base.component";
 import { CertificatesComponent } from "./pages/certificates/certificates.component";
 import { FormComponent } from "./pages/form/form.component";
 import { ListCertificateComponent } from "./pages/list-certificate/list-certificate.component";
+import { CertificatePageComponent } from "./pages/certificate-page/certificate-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, CommonModule, UiBaseComponent, CertificatesComponent, ItemListComponent, FormComponent, ListCertificateComponent],
+  imports: [NavbarComponent, CommonModule, UiBaseComponent, CertificatesComponent, ItemListComponent, FormComponent, ListCertificateComponent, CertificatePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
