adabd7354345bf5401fa9202514b243d8e675ea8
gustavogss <gustavogss.dev@gmail.com>
2025-06-15 03:24:33 -0300

    feat: add router-links

--- src/app/_components/item-list/item-list.component.ts
+++ src/app/_components/item-list/item-list.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { SecondButtonComponent } from "../second-button/second-button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-item-list',
-  imports: [SecondButtonComponent],
+  imports: [SecondButtonComponent, RouterLink],
   templateUrl: './item-list.component.html',
   styleUrl: './item-list.component.css'
 })
 export class ItemListComponent {
-
+  id: string = '5'
 }
